f0b31510b13f2fe2decb59b7330fdbdd75f314b4
Tenshiki <tenshiki.jp@gmail.com>
2025-03-26 16:39:17 +0900

    Update start.js

--- start.js
+++ start.js
@@ -5,13 +5,13 @@
 ░█─── ░█─░█ ░█─░█ ░█▄▄█ ░█─░█ ─░█── ░█─░█ ── ░█──░█ ░█▄▄▀
 
 
-* @project_name PRABATH-MD
-* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
+* @project_name SCXR
+* @authors SCXR
 * @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
-* @link <https://github.com/prabathLK/PRABATH-MD>
-* @version 2.0.6
+* @link https://github.com/Tenshiki>
+* @version 3.0.6
 
-© 2023 Prabath Kumara, SACHI-BOT. All rights reserved.
+© 2025 The Unfinish Bot . All rights reserved.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.*/
 
